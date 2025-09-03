e643a99683a75af56b8f4601a38c2e71e76b61f8
Anamika Kushwaha <aiml23058@glbitm.ac.in>
2025-09-03 21:28:34 +0530

    SelectionSort.cpp

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -1,25 +1,37 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-void SelectionSort(vector<int> arr , int n){
-    for(int i=0 ; i>n-1 ; i++){
-        minIndex=1;
-        for(int j=0 ; j<n ; j++){
-            if(arr[j] < arr[minIndex])
-            minIndex=j;
-        }swap(arr[minIndex],arr[j]);
+void SelectionSort(vector<int>& arr, int n) {
+    for (int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(arr[i], arr[minIndex]); /
     }
 }
 
-int main(){
-    int n=sizeof(arr) / sizeof(arr[0]);
-    vector<int> arr;
-    for(int i=0 ; i<n ; i++){
-        cin>>arr[i];
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter array elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
     }
 
-    cout<<SelectionSort(arr,n)<<endl;
+    SelectionSort(nums, n);
+
+    cout << "Sorted Array: ";
+    for (int i = 0; i < n; i++) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
