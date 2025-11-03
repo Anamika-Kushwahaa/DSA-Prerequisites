885207c604638fa44ae9fb0a4ab24ec50a32f1bf
Anamika Kushwaha <aiml23058@glbitm.ac.in>
2025-11-03 23:53:02 +0530

    Reverse Integer.cpp

---
+++ Reverse Integer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, rev = 0;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    while (n != 0) {
+        int digit = n % 10;   // get last digit
+        rev = rev * 10 + digit; // add it to reversed number
+        n /= 10;               // remove last digit
+    }
+
+    cout << "Reversed number: " << rev << endl;
+    return 0;
+}
